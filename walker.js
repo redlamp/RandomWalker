ba1da7b9ab8765ddac5d9d8f87bbb942b40b6bf4
Taylor Wright <taylor@redlamp.org>
2015-11-03 12:14:20 -0800

    Now we're walking!
    • Walkers are walking.
    • Lines seems to redraw each frame, causing a gradual alpha burn in
    (need to try clearing canvas).
    • Inactive walkers are culled from draw list.
    • Updated canvas background color.

--- walker.js
+++ walker.js
@@ -1,46 +1,68 @@
 var WalkerMain = function () {
     this.c = document.getElementById("walkerCanvas");
-    var ctx = c.getContext("2d");
+    this.ctx = c.getContext("2d");
+
+    this.startX = Math.floor(c.width / 2);
+    this.startY = Math.floor(c.height / 2);
     
-    var startX = Math.floor(c.width/2);
-    var startY = Math.floor(c.height/2);
+    this.walkerCount = 10;
+    this.stepDistance = 6;
+    this.lineWidth = 4;
     
-    var walkerCount = 10;
-    var stepDistance = 6;
-    var strokeWidth = 4;
+    this.walkers = createWalkers(walkerCount);
+    this.activeWalkers = walkers.slice();
     
-    var walkers = createWalkers(walkerCount);
+    draw();
 };
+
+var draw = function() {
+    console.log("draw()");
+
+    if(activeWalkers.length>0) {
+        requestAnimationFrame(draw)
+    }
+    
+    for(var i = 0; i < activeWalkers.length; i++) {
+        w = activeWalkers[i];
+        w.step();
+        w.drawStep();
+        if(!w.active){
+            console.log(w.id+": removed");
+            activeWalkers.splice(i,1);
+        }
+    }
+}
 
 var createWalkers = function(count) {
     var a = [];
     for (var i = 0; i < count; i++) {
         walker = new Walker(i);
         a.push(walker);
-        walker.step();
-        walker.draw();
     }
     return a;
 }
 
+///////////////
+//  WALKER   //
+///////////////
 var Walker = function (id) {
     this.id = id;
     var xPos = yPos = lastDrawnStep = 0;
     this.steps = [new Point()];
-    var lastStep = this.getStep;
-    var complete = false;
-    var activeColor = "Magenta";
-    var completeColor = "Purple";
+    this.lastStep = this.getStep;
+    this.active = true;
+    this.activeColor = "rgba(255, 0, 255, 0.005)";
+    this.completeColor = "rgba(102, 17, 102, 0.005)";
 };
 
 Walker.prototype.step = function() {
-    console.log(this.id+": step()");
+//    console.log(this.id+": step()");
     
     var dirs = [0, 1, 2, 3];
     
-    // TODO: Remove restricted directions
+//    if()
     
-    var dir = dirs[Math.floor(Math.random()*dirs.length)];
+    var dir = this.lastDir = dirs[Math.floor(Math.random()*dirs.length)];
     var p = this.getStep().clone();
     
     switch (dir){
@@ -65,6 +87,10 @@ Walker.prototype.step = function() {
     }
     
     this.steps.push(p);
+    
+    if(p.x === 0 && p.y === 0){
+        this.active = false;
+    }
 }
 
 Walker.prototype.getStep = function (step) {
@@ -73,16 +99,48 @@ Walker.prototype.getStep = function (step) {
     return this.steps[s];
 }
 
-Walker.prototype.draw = function () {
-    console.log(this.id+": draw()");
-    console.log("\tc: "+WalkerMain.c);
-//    c.lineTo(startX + this.lastStep.x * stepDistance);
+Walker.prototype.drawStep = function (step) {
+//    console.log(this.id+": drawStep("+step+")");
+    step = step || this.steps.length - 1;
+    var x, y;
+    l = this.getStep(step-1);
+    s = this.getStep(step);
+    
+    x = startX + l.x * stepDistance;
+    y = startY + l.y * stepDistance;
+    ctx.moveTo(x, y);
+    
+    x = startX + s.x * stepDistance;
+    y = startY + s.y * stepDistance;
+    ctx.lineTo(x, y);
+    
+    ctx.lineCap = "square";
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = this.activeColor;
+//    ctx.globalAlpha = 0.01;
+    ctx.stroke();
 }
 
-function Point(x, y){
+//////////////
+//  POINT   //
+//////////////
+var Point = function(x, y){
 	this.x = x || 0;
 	this.y = y || 0;
 };
 Point.prototype.clone = function(){
 	return new Point(this.x, this.y);
 };
+
+//////////////
+//  UTIL    //
+//////////////
+var stepToX = function (step) {
+    s = this.getStep(step);
+    x = startX + s.x * stepDistance;
+}
+
+var stepToY = function (step) {
+    s = this.getStep(step);
+    x = startY + s.y * stepDistance;
+}
